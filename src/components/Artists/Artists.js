e255344c15135e4b7d8b0b51217c54f20df84dad
Paul Carillion <pcarillion@yahoo.fr>
2020-05-20 16:34:43 +0200

    new try for prod

--- src/components/Artists/Artists.js
+++ src/components/Artists/Artists.js
@@ -14,7 +14,7 @@ query {
           contentful_id
           images{
             fluid{
-                ...GatsbyContentfulFluid_tracedSVG
+                ...GatsbyContentfulFluid
             }
           }
         }
